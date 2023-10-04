ebe0cacecc7f2c878d6e14bca7095a0059d9481b
Mitun Suresh <mitunpokkisham@gmail.com>
2023-10-04 09:59:02 +0530

    archi update

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:ayu_hub/features/ui/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        theme: ThemeData(fontFamily: 'Poppins'),
+        home: const HomeScreen(),
+      ),
+    );
