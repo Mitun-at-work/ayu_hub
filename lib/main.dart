826d0404f13478c018d1dad8033b2310a24ab18c
sudharsan-15 <sudharsans21ec@psnacet.edu.in>
2023-10-05 12:41:36 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:ayu_hub/features/ui/profile/Profile.dart';
+import 'package:ayu_hub/features/ui/login_page/login_page.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Poppins'),
-        home: const Profile(),
+        home: const LoginPage(),
       ),
     );
